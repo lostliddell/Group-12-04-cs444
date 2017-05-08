255e2ac828cc593d41aa744960a589d7814c5f4f
danielc6754 <choimi@oregonstate.edu>
2017-05-08 16:54:23 -0700

    Added twister and rdrand codes
    gcc not recognizing _rdrand32_step

--- Project2/diningphilo.c
+++ Project2/diningphilo.c
@@ -2,12 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
+#include <limits.h>
+#include <immintrin.h>
+#include <stdint.h>
 
 // Philosopher States
 #define THINKING  0
 #define HUNGRY  1
 #define EATING  2
 
+/* Period parameters */  
+#define N 624
+#define M 397
+#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
+#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
+
+static unsigned long mt[N]; /* the array for the state vector  */
+static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
+
+/* Philosopher paramters */
 pthread_mutex_t bowl; // Main mutex
 
 struct philo_Struct {
@@ -16,7 +31,108 @@ struct philo_Struct {
 	int blocktime; // Time philosopher is blocked
 } people[5];
 
-// ADD GENRAND AND INLINE ASM CHECK
+/*START OF MERSAND TWISTER CODE BLOCK*/
+/* initializes mt[N] with a seed */
+void init_genrand(unsigned long s)
+{
+   mt[0]= s & 0xffffffffUL;
+   for (mti=1; mti<N; mti++) {
+      mt[mti] = 
+	 (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
+      /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+      /* In the previous versions, MSBs of the seed affect   */
+      /* only MSBs of the array mt[].                        */
+      /* 2002/01/09 modified by Makoto Matsumoto             */
+      mt[mti] &= 0xffffffffUL;
+      /* for >32 bit machines */
+   }
+}
+
+/* generates a random number on [0,0xffffffff]-interval */
+unsigned long genrand_int32(void)
+{
+   unsigned long y;
+   static unsigned long mag01[2]={0x0UL, MATRIX_A};
+   /* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+   if (mti >= N) { /* generate N words at one time */
+      int kk;
+
+      if (mti == N+1)   /* if init_genrand() has not been called, */
+	 init_genrand(5489UL); /* a default initial seed is used */
+
+      for (kk=0;kk<N-M;kk++) {
+	 y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+	 mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+      }
+      for (;kk<N-1;kk++) {
+	 y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+	 mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+      }
+      y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
+      mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+
+      mti = 0;
+   }
+
+   y = mt[mti++];
+
+   /* Tempering */
+   y ^= (y >> 11);
+   y ^= (y << 7) & 0x9d2c5680UL;
+   y ^= (y << 15) & 0xefc60000UL;
+   y ^= (y >> 18);
+
+
+   return y;
+}
+
+// ASM suppport check
+_Bool asmCheck( unsigned int* eax, unsigned int* ebx, unsigned int* ecx, unsigned int* edx ){
+
+   __asm__ __volatile__("cpuid"
+	 : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+	 :  "a" (1), "c" (0)
+	 );
+
+}
+
+_Bool checkRdrand(){
+
+   unsigned int flag_Rdrand = (1 << 30);
+   unsigned int eax, ebx, ecx, edx;
+
+   asmCheck( &eax, &ebx, &ecx, &edx );
+
+   _Bool answer = ( (ecx & flag_Rdrand ) == flag_Rdrand);
+
+   return answer; 
+
+}
+
+int genrand(){
+
+   //int ifRdrand = checkRdrand();
+   int ifRdrand = 0;
+   int smallRand;
+
+   if( ifRdrand == 0 ){
+
+      unsigned int randNum = genrand_int32();
+      smallRand = randNum & INT_MAX;
+
+   } /*else if( ifRdrand == 1 ){
+
+	 uint32_t rnd = 0;
+
+	 int result = _rdrand32_step( &rnd );
+	 smallRand = rnd & INT_MAX; 
+
+	 }*/
+
+   return smallRand;
+
+}
 
 // Algorithm to figure out which forks are being used or not
 int abletoEat(int id) {
@@ -67,15 +183,13 @@ void put_forks(int id) {
 }
 
 void think(int id) {
-	// Using RAND() till genrand and asm support check are added
-	int thinkingtime = (rand() % 19) + 1;
+	int thinkingtime = (genrand() % 19) + 1;
 	printf("Philosopher %d is THINKING\n", id);
 	sleep(thinkingtime);
 }
 
 void eat(int id) {
-	// Using RAND() till genrand and asm support check are added
-	int eatingtime = (rand() % 7) + 2;
+	int eatingtime = (genrand() % 7) + 2;
 	printf("Philosopher %d is EATING\n", id );
 	sleep(eatingtime);
 }
